4b30fdd5b983249aa2f038a868ad28e8f6d0809b
Gregory Giecold <GGiecold@users.noreply.github.com>
2016-01-12 10:15:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open(path.join(here, 'README'), encoding = 'utf-8') as f:
     
 
 setup(name = 'Concurrent_AP',
-      version = '1.3',
+      version = '1.4',
       
       description = 'Scalable and parallel programming implementation of Affinity Propagation clustering',
       long_description = long_description,
