ffbc4fc56bb71655c5f0305f438a4e621dbac987
Gregory Giecold <GGiecold@users.noreply.github.com>
2017-01-16 09:38:23 -0500

    Update Concurrent_AP.py

--- Concurrent_AP.py
+++ Concurrent_AP.py
@@ -64,19 +64,11 @@ def memory():
         Holds the current values for the total, free and used memory of the system.
     """
 
-    mem_info = {}
+    mem_info = dict()
 
-    with open('/proc/meminfo') as file:
-        c = 0
-        for line in file:
-            lst = line.split()
-            if str(lst[0]) == 'MemTotal:':
-                mem_info['total'] = int(lst[1])
-            elif str(lst[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
-                c += int(lst[1])
-        mem_info['free'] = c
-        mem_info['used'] = (mem_info['total']) - c
-
+    for k, v in psutil.virtual_memory().__dict__.iteritems():
+           mem_info[k] = int(v)
+           
     return mem_info
 
 
